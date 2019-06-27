674974b0cfe178abb053725acb9fe6aa63d4d09d
Roberto Castanon <robertocastanon925@gmail.com>
2019-06-26 17:58:54 -0700

    random number appears at the start and is set for eac of the clickable gems, clicking one will add the number below to your total score. If your score matches the random number you win if it goes over you lose. Wins and losses are indicated in the console log for the time being.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,85 @@
-// var ranNum = document.querySelector('randomNum')
+// var ranNum = document.querySelector("ranNum")
+var container = document.querySelector('#mainContainer')
+
 // var wins = document.querySelector('wins')
 // var losses = document.querySelector('losses')
 
-// var gem1 = document.querySelector('#gem1')
-// var gem2 = document.querySelector('#gem2')
-// var gem3 = document.querySelector('#gem3')
-// var gem4 = document.querySelector('#gem4')
+var gem1 = document.querySelector('#gem1')
+var gem2 = document.querySelector('#gem2')
+var gem3 = document.querySelector('#gem3')
+var gem4 = document.querySelector('#gem4')
 
-// // var gems = [gem1, gem2, gem3, gem4]
+var totalScore = document.querySelector('#totalScore')
 
-// // function randomGenerator(min, max) {
-// //   return Math.floor(Math.random() * (max - min) ) + min
-// // }
+totalScore = 0
 
-// ranGem = Math.floor(Math.random() * 12) + 1
+function getRndInteger (min, max) {
+  return Math.floor(Math.random() * (max - min)) + min
+}
 
-// // ranNum.innerHTML = Math.floor(Math.random() * 120) + 19
+function start () {
+  var ranNum = document.querySelector('#ranNum').innerHTML = getRndInteger(19, 120)
 
-// // ranNum.innerHTML = 5
+  console.log('Random Number: ' + ranNum)
 
-// // document.ranNum.innerHTML = 2222
-// document.getElementById('randomNum').innerHTML = Math.floor(Math.random() * 120) + 19
-// // RandomNumber function () {
-// // }
+  gem1 = getRndInteger(1, 12)
+  gem2 = getRndInteger(1, 12)
+  gem3 = getRndInteger(1, 12)
+  gem4 = getRndInteger(1, 12)
 
-// document.querySelector('#gem1').addEventListener('click', function () {
-// ranGem = Math.floor(Math.random() * 12) + 1
+  function display() {
+    console.log('totalScore: ' + totalScore)
+    document.querySelector('#totalScore').innerHTML = totalScore
+  }
 
-//     console.log(ranGem)
-// })
+  function check() {
+
+    if (totalScore === ranNum) {
+      // document.querySelector('#wins').innerHTML = '1'
+      return console.log('u win')
+    } else if (totalScore > ranNum) {
+      // document.querySelector('#losses').innerHTML = '1'
+      return console.log('u lose')
+    }
+  }
+
+  function end () {
+    document.querySelector('#mainContainer')
+
+  }
+
+  document.querySelector('#gem1').addEventListener('click', function () {
+    console.log('gem1 says: ' + gem1)
+    totalScore = totalScore + gem1
+
+    display()
+    check()
+    document.getElementById('gem1').disabled = true
+  })
+  document.querySelector('#gem2').addEventListener('click', function () {
+    console.log('gem2 says: ' + gem2)
+    totalScore = totalScore + gem2
+
+    display()
+    check()
+
+  })
+  document.querySelector('#gem3').addEventListener('click', function () {
+    console.log('gem3 says: ' + gem3)
+    totalScore = totalScore + gem3
+
+    display()
+    check()
+
+  })
+  document.querySelector('#gem4').addEventListener('click', function () {
+    console.log('gem4 says: ' + gem4)
+    totalScore = totalScore + gem4
+
+    display()
+    check()
+
+  })
+}
+
+start()
